305c8d444f5fde5783cc574bdc79b06d5db50bf7
Robert Hildebrandt <robert-42@web.de>
2015-11-23 18:55:23 +0100

    started implementing sample for drawing a single quad

---
+++ samples/single-quad/main.cpp
@@ -0,0 +1,26 @@
+#include <glrt/application.h>
+#include <glrt/gui/toolbar.h>
+
+
+int main(int argc, char** argv)
+{
+  glrt::Application app(argc, argv);
+
+  while(app.isRunning())
+  {
+    sf::Event event;
+    while(app.pollEvent(event))
+    {
+    }
+
+    app.update();
+
+    app.beginDraw();
+
+    // TODO
+
+    app.endDraw();
+  }
+
+  return 0;
+}
