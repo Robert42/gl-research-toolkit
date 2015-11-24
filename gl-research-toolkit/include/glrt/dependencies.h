6d38c4617f75e15cdb00874174b94a1a35e74cfb
Robert Hildebrandt <robert-42@web.de>
2015-11-24 22:15:08 +0100

    added exceptions

--- gl-research-toolkit/include/glrt/dependencies.h
+++ gl-research-toolkit/include/glrt/dependencies.h
@@ -17,17 +17,19 @@
 
 namespace glrt {
 
-class Dependencies final
+class Exception final
 {
 public:
-  Dependencies();
+  Exception(const char* file, int line, const char* function, const QString& message);
 };
 
 } // namespace glrt
 
+#define GLRT_EXCEPTION(message) Exception(__FILE__, __LINE__, __FUNCTION__, message)
+
 #define __CALL_SDL(x, action) if(!(x)){qCritical() << #x << " failed: " << SDL_GetError() << "\n("<<__FILE__<<", " << __LINE__ << ")";action}
 #define CALL_SDL(x) __CALL_SDL(x,)
-#define CALL_SDL_CRITICAL(x) __CALL_SDL(x,exit(-1);)
+#define CALL_SDL_CRITICAL(x) __CALL_SDL(x,abort();)
 
 #define PRINT_VALUE(x) qDebug() << "Value of `" << #x << "` = " << x;
 
--- gl-research-toolkit/src/glrt/dependencies.cpp
+++ gl-research-toolkit/src/glrt/dependencies.cpp
@@ -2,9 +2,10 @@
 
 namespace glrt {
 
-Dependencies::Dependencies()
+Exception::Exception(const char* file, int line, const char* function, const QString& message)
 {
-
+  qCritical() << "Exception thrown: " << message << "\n("<<function << " in " << file << " line:" << line << ")";
+  abort();
 }
 
 } // namespace glrt
