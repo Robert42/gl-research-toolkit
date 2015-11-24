5aac09e1ad7408127a13fb4169ce5ea586941af5
Robert Hildebrandt <robert-42@web.de>
2015-11-24 20:24:12 +0100

    started implementing loading code for the mesh [CURRENTLY NOT WORKING] because of missing assets

--- samples/single-mesh/main.cpp
+++ samples/single-mesh/main.cpp
@@ -3,10 +3,37 @@
 
 #include <glhelper/gl.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+
 
 int main(int argc, char** argv)
 {
   glrt::Application app(argc, argv, glrt::System::Settings::simpleWindow("Single Mesh"));
+
+  Assimp::Importer importer;
+  const aiScene* scene = importer.ReadFile(GLRT_ASSET_DIR"/meshes/suzanne.obj",
+                                           // aiProcess_RemoveComponent |  TODO: use this
+                                           aiProcess_JoinIdenticalVertices | // TODO: this can be used to allow switching between indexed and not indexed mode
+                                           aiProcess_PreTransformVertices | // TODO: have this only activated when loading a single mesh
+                                           aiProcess_CalcTangentSpace |
+                                           aiProcess_GenNormals |
+                                           aiProcess_ValidateDataStructure |
+                                           aiProcess_RemoveRedundantMaterials |
+                                           aiProcess_OptimizeMeshes |
+                                           aiProcess_OptimizeGraph |
+                                           aiProcess_ImproveCacheLocality |
+                                           aiProcess_SortByPType  | // TODO Read the documentation to this
+                                           aiProcess_FindDegenerates  |
+                                           aiProcess_FindInvalidData  |
+                                           aiProcess_GenUVCoords  | // TODO: think about this
+                                           aiProcess_Triangulate
+                                           );
+  if(!scene)
+  {
+    qCritical() << "Couldn't load mesh: " << importer.GetErrorString();
+    exit(-1);
+  }
 
   while(app.isRunning)
   {
