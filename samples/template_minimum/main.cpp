89ab3a5a3e20310dd673631778b97ed192058e9c
Robert Hildebrandt <robert-42@web.de>
2015-11-24 18:39:34 +0100

    added template for minimum working appliction

---
+++ samples/template_minimum/main.cpp
@@ -0,0 +1,28 @@
+#include <glrt/application.h>
+#include <glrt/gui/toolbar.h>
+
+#include <glhelper/gl.hpp>
+
+
+int main(int argc, char** argv)
+{
+  glrt::Application app(argc, argv, glrt::System::Settings::simpleWindow("TemplateMinimum"));
+
+  while(app.isRunning)
+  {
+    SDL_Event event;
+    while(app.pollEvent(&event))
+    {
+    }
+
+    app.update();
+
+    GL_CALL(glClear, GL_COLOR_BUFFER_BIT);
+
+    // T O D O : Add your drawing code here
+
+    app.swapWindow();
+  }
+
+  return 0;
+}
