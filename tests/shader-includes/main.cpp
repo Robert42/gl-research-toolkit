5c45454fb43808a3307b9c060edea9ef78b8e8e5
Robert Hildebrandt <robert-42@web.de>
2015-11-28 13:38:22 +0100

    added a shader to the shaderinclude test

--- tests/shader-includes/main.cpp
+++ tests/shader-includes/main.cpp
@@ -3,12 +3,18 @@
 
 #include <glhelper/screenalignedtriangle.hpp>
 
+QDir shaderDir(SHADER_DIR);
 
 int main(int argc, char** argv)
 {
   glrt::Application app(argc, argv, glrt::System::Settings::addVSync(glrt::System::Settings::simpleWindow("Shader-Includes")));
 
   gl::ScreenAlignedTriangle screenAlignedTriangle;
+
+  gl::ShaderObject shader("test-shader");
+  shader.AddShaderFromFile(gl::ShaderObject::ShaderType::FRAGMENT, shaderDir.absoluteFilePath("test-shader.fs").toStdString());
+  shader.CreateProgram();
+  shader.Activate();
 
   while(app.isRunning)
   {
