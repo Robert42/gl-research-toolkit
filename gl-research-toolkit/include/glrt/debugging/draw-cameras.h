0c64a110bd8f9a5300160d02b298d5e01bf9ab50
Robert Hildebrandt <robert-42@web.de>
2015-12-04 23:32:25 +0100

    prepared implementing class responsible for debug drawing camera positions

---
+++ gl-research-toolkit/include/glrt/debugging/draw-cameras.h
@@ -0,0 +1,17 @@
+#ifndef GLRT_DEBUGGING_DRAWCAMERAS_H
+#define GLRT_DEBUGGING_DRAWCAMERAS_H
+
+
+namespace glrt {
+namespace debugging {
+
+class DrawCameras
+{
+public:
+  DrawCameras();
+};
+
+} // namespace debugging
+} // namespace glrt
+
+#endif // GLRT_DEBUGGING_DRAWCAMERAS_H
---
+++ gl-research-toolkit/src/glrt/debugging/draw-cameras.cpp
@@ -0,0 +1,13 @@
+#include <glrt/debugging/draw-cameras.h>
+
+namespace glrt {
+namespace debugging {
+
+DrawCameras::DrawCameras()
+{
+
+}
+
+} // namespace debugging
+} // namespace glrt
+
