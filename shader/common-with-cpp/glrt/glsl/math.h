68b7e19301ca8b4d89eb55b879b7217bb31a7f19
Robert Hildebrandt <robert-42@web.de>
2015-12-08 23:08:50 +0100

    added helper functions

--- shader/common-with-cpp/glrt/glsl/math.h
+++ shader/common-with-cpp/glrt/glsl/math.h
@@ -22,4 +22,26 @@ float sq(vec4 x)
   return dot(x, x);
 }
 
+// blending
+
+float blend_screen(float x,float y)
+{
+  return 1.f-(1.f-x)*(1.f-y);
+}
+
+vec2 blend_screen(vec2 x, vec2 y)
+{
+  return vec2(1)-(vec2(1)-x)*(vec2(1)-y);
+}
+
+vec3 blend_screen(vec3 x, vec3 y)
+{
+  return vec3(1)-(vec3(1)-x)*(vec3(1)-y);
+}
+
+vec4 blend_screen(vec4 x, vec4 y)
+{
+  return vec4(1)-(vec4(1)-x)*(vec4(1)-y);
+}
+
 #include <glrt/glsl/compatibility/end.h>
