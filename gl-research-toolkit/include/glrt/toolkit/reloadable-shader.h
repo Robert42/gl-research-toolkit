371eb2614aad0224337e62a3693ac7ecddf1ba34
Robert Hildebrandt <robert-42@web.de>
2015-12-11 21:30:20 +0100

    addded Reloadable shader

---
+++ gl-research-toolkit/include/glrt/toolkit/reloadable-shader.h
@@ -0,0 +1,22 @@
+#ifndef GLRT_RELOADABLESHADER_H
+#define GLRT_RELOADABLESHADER_H
+
+#include <glrt/dependencies.h>
+
+namespace glrt {
+
+class ReloadableShader final
+{
+public:
+  ReloadableShader();
+  ~ReloadableShader();
+
+  ReloadableShader(const ReloadableShader&) = delete;
+  ReloadableShader(ReloadableShader&&) = delete;
+  ReloadableShader& operator=(const ReloadableShader&) = delete;
+  ReloadableShader& operator=(ReloadableShader&&) = delete;
+};
+
+} // namespace glrt
+
+#endif // GLRT_RELOADABLESHADER_H
---
+++ gl-research-toolkit/src/glrt/toolkit/reloadable-shader.cpp
@@ -0,0 +1,17 @@
+#include <glrt/toolkit/reloadable-shader.h>
+
+namespace glrt {
+
+
+ReloadableShader::ReloadableShader()
+{
+}
+
+
+ReloadableShader::~ReloadableShader()
+{
+}
+
+
+} // namespace glrt
+
