a18dd0404b6b2dcdd612c6128d0f3b0d40f2fff4
Robert Hildebrandt <robert-42@web.de>
2015-12-12 09:34:21 +0100

    started implementing Logger

---
+++ gl-research-toolkit/include/glrt/toolkit/logger.h
@@ -0,0 +1,15 @@
+#ifndef GLRT_LOGGER_H
+#define GLRT_LOGGER_H
+
+
+namespace glrt {
+
+class Logger
+{
+public:
+  Logger();
+};
+
+} // namespace glrt
+
+#endif // GLRT_LOGGER_H
---
+++ gl-research-toolkit/src/glrt/toolkit/logger.cpp
@@ -0,0 +1,11 @@
+#include <glrt/toolkit/logger.h>
+
+namespace glrt {
+
+Logger::Logger()
+{
+
+}
+
+} // namespace glrt
+
