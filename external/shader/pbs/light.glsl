d7c88fb3fffa17b3597cecbba980bd9ae9446a3f
Robert Hildebrandt <robert-42@web.de>
2015-12-15 00:22:48 +0100

    added the snippet for the rectangle solid angle

--- external/shader/pbs/light.glsl
+++ external/shader/pbs/light.glsl
@@ -1,3 +1,25 @@
+float rectangleSolidAngle(float3 worldPos,
+                          float3 p0, float3 p1,
+                          float3 p2, float3 p3)
+{
+    float3 v0 = p0 - worldPos;
+    float3 v1 = p1 - worldPos;
+    float3 v2 = p2 - worldPos;
+    float3 v3 = p3 - worldPos;
+
+    float3 n0 = normalize(cross(v0, v1));
+    float3 n1 = normalize(cross(v1, v2));
+    float3 n2 = normalize(cross(v2, v3));
+    float3 n3 = normalize(cross(v3, v0));
+
+    float g0 = acos(dot(-n0, n1));
+    float g1 = acos(dot(-n1, n2));
+    float g2 = acos(dot(-n2, n3));
+    float g3 = acos(dot(-n3, n0));
+
+    return g0 + g1 + g2 + g3 - 2 * FB_PI;
+}
+
 float rectAreaLightLuminance(in vec3 worldPos, in vec3 worldNormal, in vec3 lightPos, in vec3 lightPlaneNormal, in vec3 lightLeft, in vec3 lightUp, in float halfWidth, in float halfHeight)
 {
   float illuminance = 0.f;
