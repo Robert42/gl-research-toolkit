aa07a99e29e109e645afbb33d5956004b30d1d34
Robert Hildebrandt <robert-42@web.de>
2015-12-16 17:14:21 +0100

    using now the diffuse color

--- external/shader/pbs/material.glsl
+++ external/shader/pbs/material.glsl
@@ -37,7 +37,7 @@ vec3 material_brdf(in BrdfParameters brdf_param, in BaseMaterial material)
   vec3 f_s = brdf_specular(brdf_param, roughness, f0, f90);
   float f_d = brdf_diffuse(brdf_param, roughness);
   
-  return f_s * specular_occlusion + f_d * diffuse_occlusion;
+  return f_s * specular_occlusion + f_d * diffuse_occlusion * diffuseColor;
 }
 
 vec3 material_brdf(in vec3 V, in vec3 L, in BaseMaterial material)
