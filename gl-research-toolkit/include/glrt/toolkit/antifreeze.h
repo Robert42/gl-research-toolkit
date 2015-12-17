650d31fc2f81a30ae9949bd09b3d58bdf307eb50
Robert Hildebrandt <robert-42@web.de>
2015-12-17 12:39:37 +0100

    started implementing the antifreeze

--- gl-research-toolkit/include/glrt/toolkit/antifreeze.h
+++ gl-research-toolkit/include/glrt/toolkit/antifreeze.h
@@ -8,14 +8,49 @@ namespace glrt {
 class Antifreeze
 {
 public:
-  Antifreeze();
+  class Sleep;
+
+  explicit Antifreeze(int numberSecondsToWait=5);
   ~Antifreeze();
 
   Antifreeze(Antifreeze&&) = delete;
   Antifreeze(const Antifreeze&) = delete;
-  Antifreeze&operator=(Antifreeze&&) = delete;
-  Antifreeze&operator=(const Antifreeze&) = delete;
+  Antifreeze& operator=(Antifreeze&&) = delete;
+  Antifreeze& operator=(const Antifreeze&) = delete;
+
+  void nextFrame();
+
+private:
+  struct Data
+  {
+    std::atomic<quint32> frame_id;
+    int numberSecondsToWait = 5;
+    bool running;
+  };
+
+  Data* data;
+
+  static void prevent_freezes(Data* data);
 };
+
+
+class Antifreeze::Sleep
+{
+public:
+  Sleep();
+  ~Sleep();
+
+  Sleep(Sleep&&) = delete;
+  Sleep(const Sleep&) = delete;
+  Sleep& operator=(Sleep&&) = delete;
+  Sleep& operator=(const Sleep&) = delete;
+
+  static bool isSleeping();
+
+private:
+  static std::atomic<quint32> number_sleeps;
+};
+
 
 } // namespace glrt
 
--- gl-research-toolkit/src/glrt/toolkit/antifreeze.cpp
+++ gl-research-toolkit/src/glrt/toolkit/antifreeze.cpp
@@ -1,14 +1,75 @@
 #include <glrt/toolkit/antifreeze.h>
 
+/*! \class glrt::Antifreeze
+
+Workaround to prevents some drivers to freeze because of an endless loop within a shader.
+
+*/
+
 namespace glrt {
 
+// ======== Antifreeze =========================================================
 
-Antifreeze::Antifreeze()
+Antifreeze::Antifreeze(int numberSecondsToWait)
+  : data(new Data)
 {
+  data->numberSecondsToWait = numberSecondsToWait;
+
+  std::thread thread(std::bind(prevent_freezes, data));
+  thread.detach();
 }
 
 Antifreeze::~Antifreeze()
 {
+  data->running = true;
+  data = nullptr;
+}
+
+void Antifreeze::nextFrame()
+{
+  data->frame_id++;
+}
+
+void Antifreeze::prevent_freezes(Data* data)
+{
+  std::chrono::seconds sleepTime(data->numberSecondsToWait);
+
+  quint32 last_id = data->frame_id;
+
+  while(data->running)
+  {
+    std::this_thread::sleep_for(sleepTime);
+
+    if(last_id == data->frame_id && !Sleep::isSleeping())
+    {
+      qCritical() << "Antifreeze detected a freeze!";
+      abort();
+      break;
+    }
+
+    last_id = data->frame_id;
+  }
+
+  delete data;
+}
+
+// ======== Sleep ==============================================================
+
+std::atomic<quint32> Antifreeze::Sleep::number_sleeps;
+
+Antifreeze::Sleep::Sleep()
+{
+  number_sleeps++;
+}
+
+Antifreeze::Sleep::~Sleep()
+{
+  number_sleeps--;
+}
+
+bool Antifreeze::Sleep::isSleeping()
+{
+  return number_sleeps > 0;
 }
 
 
