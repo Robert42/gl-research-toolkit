f6df3d5c8c2e5cd634edadfa434c134ca392bafe
Robert Hildebrandt <robert-42@web.de>
2015-12-20 00:18:17 +0100

    using now tonemaping

--- shader/materials/implementation/forward-lighting.fs.glsl
+++ shader/materials/implementation/forward-lighting.fs.glsl
@@ -8,7 +8,15 @@ out vec4 color;
 
 void apply_material(in BaseMaterial material, in SurfaceData surface, float alpha)
 {
-  color = vec4(light_material(material, surface.position, scene.camera_position), alpha);
+  vec3 incoming_luminance = light_material(material, surface.position, scene.camera_position);
+  
+  float exposure = 1.f; // Only dummy value, to be corrected
+  
+  incoming_luminance *= exposure;
+  
+  color = vec4(accurateLinearToSRGB(incoming_luminance), alpha);
+  
+  
 }
 
 #endif // FORWARD_RENDERER
