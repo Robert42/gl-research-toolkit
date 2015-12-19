e027775e594bffe79c7aaec96ab675962abdc77b
Robert Hildebrandt <robert-42@web.de>
2015-12-19 11:56:52 +0100

    continue debugging

--- shader/toolkit/lighting/rendering-equation.glsl
+++ shader/toolkit/lighting/rendering-equation.glsl
@@ -101,7 +101,6 @@ vec3 rendering_equation(in BrdfData_Generic brdf_g, in SurfaceData surface)
     light_data.illuminance = rectAreaLightIlluminance(worldPosition, worldNormal, rect);
     light_data.direction_to_light = getDirectionToLight(light_data.specularEnergyFactor, rect, surface);
     
-    // TODO: muoltiply instead of if?
     outgoing_light += do_the_lighting(light_data, brdf_g, surface);
   }
   
