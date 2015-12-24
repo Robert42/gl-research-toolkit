193e1c13db00322a68dc06eec646a40f639d0645
Robert Hildebrandt <robert-42@web.de>
2015-12-25 00:25:25 +0100

    prepared CMake project to split the renderer and the scene description

--- update-cmake.sh
+++ update-cmake.sh
@@ -8,7 +8,11 @@ cd ./external/shader
 ./update-cmake.sh
 cd - > /dev/null
 
-cd ./gl-research-toolkit
+cd ./gl-research-toolkit/scene
+./update-cmake.sh
+cd - > /dev/null
+
+cd ./gl-research-toolkit/renderer
 ./update-cmake.sh
 cd - > /dev/null
 
