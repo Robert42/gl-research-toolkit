95354e7d94cba1b5bec5bcab9ef95f271aebf478
Robert Hildebrandt <robert-42@web.de>
2016-01-08 14:30:09 +0100

    started implementing tests for the custom array container type

---
+++ tests/arrays/main.cpp
@@ -0,0 +1,17 @@
+#include <glrt/toolkit/podarray.h>
+
+#include <testing-framework.h>
+
+void test_add_remove()
+{
+}
+
+
+int main(int argc, char** argv)
+{
+  TestingApplication testing_application(argc, argv);
+
+  test_add_remove();
+
+  return testing_application.result();
+}
