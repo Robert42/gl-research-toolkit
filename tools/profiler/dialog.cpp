71471dc1f3a632b7ee1cbfbd38b85bace872cec9
Robert Hildebrandt <robert-42@web.de>
2016-01-11 09:41:46 +0100

    refactoring

--- tools/profiler/dialog.cpp
+++ tools/profiler/dialog.cpp
@@ -10,6 +10,7 @@ Dialog::Dialog(QWidget *parent) :
   ui->spinBox_Port->setValue(GLRT_PROFILER_DEFAULT_PORT);
 
   on_btnStartServer_clicked();
+  ui->tabWidget_Games->clear();
 }
 
 Dialog::~Dialog()
@@ -19,16 +20,19 @@ Dialog::~Dialog()
 
 void Dialog::on_btnStartServer_clicked()
 {
-  ui->btnStartServer->setEnabled(false);
-  ui->btnStopServer->setEnabled(true);
-  ui->spinBox_Port->setEnabled(false);
   server.listen(QHostAddress::LocalHost, ui->spinBox_Port->value());
+  update_widgets_to_connection_state();
 }
 
 void Dialog::on_btnStopServer_clicked()
 {
-  ui->btnStartServer->setEnabled(true);
-  ui->btnStopServer->setEnabled(false);
-  ui->spinBox_Port->setEnabled(true);
   server.close();
+  update_widgets_to_connection_state();
+}
+
+void Dialog::update_widgets_to_connection_state()
+{
+  ui->btnStartServer->setEnabled(!server.isListening());
+  ui->btnStopServer->setEnabled(server.isListening());
+  ui->spinBox_Port->setEnabled(!server.isListening());
 }
--- tools/profiler/dialog.h
+++ tools/profiler/dialog.h
@@ -24,6 +24,7 @@ private:
 private slots:
   void on_btnStartServer_clicked();
   void on_btnStopServer_clicked();
+  void update_widgets_to_connection_state();
 };
 
 #endif // DIALOG_H
