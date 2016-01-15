d712f90dc512b230b008741f5743fe558f151868
Robert Hildebrandt <robert-42@web.de>
2016-01-15 16:58:49 +0100

    made the cmake files more stable

--- external/shader/update-cmake.sh
+++ external/shader/update-cmake.sh
@@ -4,7 +4,7 @@ export FILE=CMakeLists.txt
 
 echo -e "cmake_minimum_required(VERSION 3.1 FATAL_ERROR)\n\nadd_custom_target(\n  gl-research-external-shader\n  SOURCES" > $FILE
 
-find . | grep -E '\.(vs|fs|glsl|h)$' | sort | sed -E 's/^\.\//  /g' >> $FILE
+find . | grep -E '\.(vs|fs|glsl|h)$' | sort -d | sed -E 's/^\.\//  /g' >> $FILE
 
 echo ")" >> $FILE
 
