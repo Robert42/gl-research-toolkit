29d4668827fa503335bb2ecdd3785071260869b9
Robert Hildebrandt <robert-42@web.de>
2016-01-24 11:33:37 +0100

    updated files.cmake

--- gl-research-toolkit/core/files.cmake
+++ gl-research-toolkit/core/files.cmake
@@ -1,14 +1,19 @@
 set(SOURCE_FILES
   include/glrt/dependencies.h
+  include/glrt/toolkit/array.h
+  include/glrt/toolkit/array.inl
   include/glrt/toolkit/assimp-glm-converter.h
+  include/glrt/toolkit/escape-string.h
   include/glrt/toolkit/geometry.h
-  include/glrt/toolkit/json.h
+  include/glrt/toolkit/linked-tuple.h
+  include/glrt/toolkit/logger.h
   include/glrt/toolkit/network.h
   include/glrt/toolkit/profiler.h
   include/glrt/toolkit/uuid.h
   include/glrt/toolkit/uuid.inl
   src/glrt/dependencies.cpp
-  src/glrt/toolkit/json.cpp
+  src/glrt/toolkit/escape-string.cpp
+  src/glrt/toolkit/logger.cpp
   src/glrt/toolkit/network.cpp
   src/glrt/toolkit/profiler.cpp
   src/glrt/toolkit/uuid.cpp
