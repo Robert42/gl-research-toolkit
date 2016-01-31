3e8dcd0f971d3c2e8a5eab965ebc6cc990187f97
Robert Hildebrandt <robert-42@web.de>
2016-02-01 00:24:19 +0100

    added TODO

--- tests/testing-framework/testing-framework.h
+++ tests/testing-framework/testing-framework.h
@@ -5,6 +5,7 @@
 
 #include "testing-application.h"
 
+// #FIXME use gtest
 #define EXPECT_NEAR_EPSILON(value, expected, epsilon) if(glm::distance(value, expected) > epsilon){qCritical() << "Expected"<<expected<<"got"<<value<<"\nExpression for the tested value: " <<#value<<"\nExpression for the expected value: "<<#expected<<"\n";Q_UNREACHABLE();}
 #define EXPECT_NEAR(value, expected) EXPECT_NEAR_EPSILON(value, expected, 0.0001f)
 #define EXPECT_EQ(value, expected) if((value)!=(expected)){qCritical() << "Expected"<<(expected)<<"got"<<(value)<<"\nExpression for the tested value: " <<#value<<"\nExpression for the expected value: "<<#expected<<"\n";Q_UNREACHABLE();}
