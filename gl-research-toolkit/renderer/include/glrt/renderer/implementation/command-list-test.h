2856a9777c700ea91da7784d0cd14f1ca61a0046
Robert Hildebrandt <robert-42@web.de>
2016-03-02 10:40:52 +0100

    started implementing a command list test

---
+++ gl-research-toolkit/renderer/include/glrt/renderer/implementation/command-list-test.h
@@ -0,0 +1,21 @@
+#ifndef GLRT_RENDERER_COMMANDLISTTEST_H
+#define GLRT_RENDERER_COMMANDLISTTEST_H
+
+
+namespace glrt {
+namespace renderer {
+namespace implementation {
+
+
+class CommandListTest
+{
+public:
+  CommandListTest();
+};
+
+
+} // namespace implementation
+} // namespace renderer
+} // namespace glrt
+
+#endif // GLRT_RENDERER_COMMANDLISTTEST_H
---
+++ gl-research-toolkit/renderer/src/glrt/renderer/implementation/command-list-test.cpp
@@ -0,0 +1,15 @@
+#include <glrt/renderer/implementation/command-list-test.h>
+
+namespace glrt {
+namespace renderer {
+namespace implementation {
+
+
+CommandListTest::CommandListTest()
+{
+}
+
+
+} // namespace implementation
+} // namespace renderer
+} // namespace glrt
