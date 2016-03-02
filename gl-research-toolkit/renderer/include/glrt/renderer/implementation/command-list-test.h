fbb47a7acd40cbba7686e39ad84f4f9e375b1615
Robert Hildebrandt <robert-42@web.de>
2016-03-02 13:26:46 +0100

    contining implementing class for testing out workign with command lists

--- gl-research-toolkit/renderer/include/glrt/renderer/implementation/command-list-test.h
+++ gl-research-toolkit/renderer/include/glrt/renderer/implementation/command-list-test.h
@@ -1,16 +1,90 @@
 #ifndef GLRT_RENDERER_COMMANDLISTTEST_H
 #define GLRT_RENDERER_COMMANDLISTTEST_H
 
+#include <glrt/dependencies.h>
+#include <glhelper/framebufferobject.hpp>
+#include <glhelper/shaderobject.hpp>
+#include <glhelper/buffer.hpp>
+#include <glrt/renderer/gl/command-list.h>
+#include <glrt/renderer/gl/status-capture.h>
 
 namespace glrt {
 namespace renderer {
 namespace implementation {
 
 
-class CommandListTest
+class CommandListTest : public QObject
 {
+  Q_OBJECT
+
 public:
+  class AcceptGivenFramebuffer;
+  class OrangeFullscreenRect;
+  class SimpleMesh;
+  class SimpleRect;
+
+  typedef QSharedPointer<CommandListTest> Ptr;
+
+  void draw();
+
+protected:
   CommandListTest();
+
+  gl::StatusCapture statusCapture;
+  gl::CommandList commandList;
+};
+
+class CommandListTest::AcceptGivenFramebuffer : public CommandListTest
+{
+  Q_OBJECT
+
+public:
+  gl::FramebufferObject* const framebuffer;
+
+  AcceptGivenFramebuffer(gl::FramebufferObject* framebuffer);
+
+  virtual void captureStateNow(gl::StatusCapture::Mode mode);
+};
+
+class CommandListTest::SimpleMesh : public AcceptGivenFramebuffer
+{
+  Q_OBJECT
+public:
+  SimpleMesh(gl::FramebufferObject* framebuffer, const QString& shaderName);
+
+  void captureStateNow(gl::StatusCapture::Mode mode) override;
+
+protected:
+  gl::ShaderObject shader;
+
+private:
+  typedef AcceptGivenFramebuffer parent_class;
+};
+
+class CommandListTest::SimpleRect : public SimpleMesh
+{
+  Q_OBJECT
+
+public:
+  SimpleRect(gl::FramebufferObject* framebuffer, const QString& shaderName, const glm::vec2& rectMin=glm::vec2(-1, -1), const glm::vec2& rectMax=glm::vec2(1, 1));
+
+  void captureStateNow(gl::StatusCapture::Mode mode) override;
+
+private:
+  typedef SimpleMesh parent_class;
+
+  gl::Buffer buffer;
+};
+
+class CommandListTest::OrangeFullscreenRect : public SimpleRect
+{
+  Q_OBJECT
+
+public:
+  OrangeFullscreenRect(gl::FramebufferObject* framebuffer);
+
+private:
+  typedef SimpleRect parent_class;
 };
 
 
--- gl-research-toolkit/renderer/src/glrt/renderer/implementation/command-list-test.cpp
+++ gl-research-toolkit/renderer/src/glrt/renderer/implementation/command-list-test.cpp
@@ -1,4 +1,6 @@
 #include <glrt/renderer/implementation/command-list-test.h>
+#include <glrt/glsl/layout-constants.h>
+#include <glrt/toolkit/array.h>
 
 namespace glrt {
 namespace renderer {
@@ -7,6 +9,81 @@ namespace implementation {
 
 CommandListTest::CommandListTest()
 {
+}
+
+void CommandListTest::draw()
+{
+  commandList.call();
+}
+
+
+CommandListTest::AcceptGivenFramebuffer::AcceptGivenFramebuffer(gl::FramebufferObject* framebuffer)
+  : framebuffer(framebuffer)
+{
+}
+
+void CommandListTest::AcceptGivenFramebuffer::captureStateNow(gl::StatusCapture::Mode mode)
+{
+  framebuffer->Bind(false);
+  statusCapture = gl::StatusCapture::capture(mode);
+  framebuffer->BindBackBuffer();
+  gl::ShaderObject::Deactivate();
+}
+
+
+CommandListTest::SimpleMesh::SimpleMesh(gl::FramebufferObject* framebuffer, const QString& shaderName)
+  : AcceptGivenFramebuffer(framebuffer),
+    shader(shaderName.toStdString())
+{
+}
+
+void CommandListTest::SimpleMesh::captureStateNow(gl::StatusCapture::Mode mode)
+{
+  shader.Activate();
+  parent_class::captureStateNow(mode);
+  gl::ShaderObject::Deactivate();
+}
+
+
+CommandListTest::SimpleRect::SimpleRect(gl::FramebufferObject* framebuffer, const QString& shaderName, const glm::vec2& rectMin, const glm::vec2& rectMax)
+  : SimpleMesh(framebuffer, shaderName)
+{
+  const Array<float> positions = {rectMin.x, rectMin.y,
+                                  rectMin.x, rectMax.y,
+                                  rectMax.x, rectMin.y,
+                                  rectMax.x, rectMax.y};
+  buffer = std::move(gl::Buffer(positions.length()*sizeof(float), gl::Buffer::UsageFlag::IMMUTABLE, positions.data()));
+}
+
+void CommandListTest::SimpleRect::captureStateNow(gl::StatusCapture::Mode mode)
+{
+  GL_CALL(glVertexAttribFormatNV, VERTEX_ATTRIBUTE_LOCATION_POSITION, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float));
+  GL_CALL(glEnableVertexAttribArray, VERTEX_ATTRIBUTE_LOCATION_POSITION);
+  parent_class::captureStateNow(mode);
+  GL_CALL(glDisableVertexAttribArray, VERTEX_ATTRIBUTE_LOCATION_POSITION);
+}
+
+
+CommandListTest::OrangeFullscreenRect::OrangeFullscreenRect(gl::FramebufferObject* framebuffer)
+  : SimpleRect(framebuffer, "orange-fullscreen-rect")
+{
+  shader.AddShaderFromSource(gl::ShaderObject::ShaderType::VERTEX,
+                             QString("#version 450 core\n"
+                             "in layout(location=%0 vec2 position;\n"
+                             "void main()\n"
+                             "{\n"
+                             "gl_Position = vec4(position.x, position.y, 0, 1);"
+                             "}\n").arg(VERTEX_ATTRIBUTE_LOCATION_POSITION).toStdString(),
+                             "main.cpp (orange vertex)");
+  shader.AddShaderFromSource(gl::ShaderObject::ShaderType::FRAGMENT,
+                             "#version 450 core\n"
+                             "out vec4 color;\n"
+                             "void main()\n"
+                             "{\n"
+                             "color = vec4(1, 0.5, 0, 1);"
+                             "}\n",
+                             "main.cpp (orange fragment)");
+  shader.CreateProgram();
 }
 
 
