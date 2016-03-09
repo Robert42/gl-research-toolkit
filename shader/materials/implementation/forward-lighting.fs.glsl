6dda825d625cb910914e72cc64cdca13cf1c347d
Robert Hildebrandt <robert-42@web.de>
2016-03-09 22:45:15 +0100

    refactoring

--- shader/materials/implementation/forward-lighting.fs.glsl
+++ shader/materials/implementation/forward-lighting.fs.glsl
@@ -35,15 +35,16 @@ void apply_material(in BaseMaterial material, in SurfaceData surface, float alph
   return;
 #endif
 
+#ifdef DEPTH_PREPASS
+  color = vec4(vec3(0), alpha);
+  return;
+#endif
+
   vec3 incoming_luminance = light_material(material, surface.position, scene.camera_position);
   
   float exposure = 1.f; // Only dummy value, to be corrected
   
   incoming_luminance *= exposure;
-  
-#ifdef DEPTH_PREPASS
-  color = vec4(vec3(0), alpha);
-#endif
 
 #ifdef FORWARD_PASS
   color = vec4(accurateLinearToSRGB(incoming_luminance), alpha);
