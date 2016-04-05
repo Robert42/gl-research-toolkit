80b9e3f9617f407c26fe06cf706ca2c9399aa8b3
Robert Hildebrandt <robert-42@web.de>
2016-04-05 21:09:31 +0200

    added TODO notes

--- gl-research-toolkit/renderer/include/glrt/renderer/debugging/debugging-posteffect.h
+++ gl-research-toolkit/renderer/include/glrt/renderer/debugging/debugging-posteffect.h
@@ -13,8 +13,9 @@ class DebuggingPosteffect
 public:
   DebuggingPosteffect() = delete;
 
-  static DebugRenderer orangeScreen(float zValue, float radius, bool depthTest);
-  static DebugRenderer overlayLightSources();
+  static DebugRenderer orangeScreen(float zValue, float radius, bool depthTest); // #TODO
+  static DebugRenderer overlayLightSources(bool depthTest); // #TODO implement posteffect for drawing light sources (don't forget gamma)
+  // #TODO: Think about writing a debug (mesh based) renderer for lgith sources
 };
 
 } // namespace debugging
