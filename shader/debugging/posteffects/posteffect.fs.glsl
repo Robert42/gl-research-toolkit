a022c701ec554cfee10104517a91e2bf8b7bd14a
Robert Hildebrandt <robert-42@web.de>
2016-04-11 13:06:38 +0200

    added TODO comment

--- shader/debugging/posteffects/posteffect.fs.glsl
+++ shader/debugging/posteffects/posteffect.fs.glsl
@@ -12,6 +12,7 @@ in FragmentBlock
 
 float FragCoord_z_toFragDepth(float z)
 {
+  // TODO check for performance improvement by leaving out gl_DepthRange.diff and gl_DepthRange.near
   return (0.5 * z + 0.5) * gl_DepthRange.diff + gl_DepthRange.near;
 }
 
