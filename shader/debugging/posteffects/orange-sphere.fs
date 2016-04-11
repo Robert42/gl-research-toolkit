ee089eebe25c44b8e11788fed553b9c82c939a6d
Robert Hildebrandt <robert-42@web.de>
2016-04-11 21:49:30 +0200

    refactoring

--- shader/debugging/posteffects/orange-sphere.fs
+++ shader/debugging/posteffects/orange-sphere.fs
@@ -1,7 +1,7 @@
 #version 450 core
 #include "posteffect.fs.glsl"
 
-layout(std140, binding=UNIFORM_BINDING_DEBUG_POSTEFFECT) uniform PostEffectBlock
+layout(std140, binding=UNIFORM_BINDING_DEBUG_POSTEFFECT_FRAGMENT) uniform PostEffectBlock
 {
   Sphere orangeSphere;
 };
