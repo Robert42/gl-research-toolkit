78728238e70f7de1f4ce02b64f21b2bcaf76e19d
Robert Hildebrandt <robert-42@web.de>
2016-04-18 11:11:12 +0200

    removed the called if for not tetsured materials

--- external/shader/pbs/roughness-adaptation.glsl
+++ external/shader/pbs/roughness-adaptation.glsl
@@ -4,6 +4,7 @@ float adjustRoughness(float inputRoughness, float avgNormalLength)
 {
   // TODO: test, whether avoiding the if is performanter
   
+#ifdef TEXTURED
   // Based on The Order : 1886 SIGGRAPH course notes implementation
   if(avgNormalLength < 1.0f)
   {
@@ -12,5 +13,6 @@ float adjustRoughness(float inputRoughness, float avgNormalLength)
     float variance = 1.0f / (2.0 * kappa);
     return sqrt(inputRoughness * inputRoughness + variance);
   }
+#endif
   return inputRoughness;
 }
