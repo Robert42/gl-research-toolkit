ebf75b495dccb9e2bd327678ab9f51b3f09319ad
Robert Hildebrandt <robert-42@web.de>
2016-05-01 15:25:34 +0200

    added the offset

--- shader/toolkit/voxels/distance-field-utils.glsl
+++ shader/toolkit/voxels/distance-field-utils.glsl
@@ -1,6 +1,10 @@
 float distancefield_distance(vec3 voxelCoord, in WorldVoxelUvwSpaceFactor spaceFactor, sampler3D voxelTexture)
 {
-  return texture(voxelTexture, spaceFactor.voxelToUvwSpace * voxelCoord).r;
+  float voxel_value = texture(voxelTexture, spaceFactor.voxelToUvwSpace * voxelCoord).r;
+  
+  voxel_value += posteffect_param.distancefield_offset;;
+  
+  return voxel_value;
 }
 
 vec3 distancefield_normal(vec3 voxelCoord, in WorldVoxelUvwSpaceFactor spaceFactor, sampler3D texture, float epsilon)
