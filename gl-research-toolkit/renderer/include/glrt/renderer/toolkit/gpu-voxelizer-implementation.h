f7ca16ab5e11f9f0b3dd559f34a9dec6f583f74f
Robert Hildebrandt <robert-42@web.de>
2016-05-04 00:20:55 +0200

    added gpuvoxelizer implementation

---
+++ gl-research-toolkit/renderer/include/glrt/renderer/toolkit/gpu-voxelizer-implementation.h
@@ -0,0 +1,17 @@
+#ifndef GLRT_RENDERER_GPUVOXELIZERIMPLEMENTATION_H
+#define GLRT_RENDERER_GPUVOXELIZERIMPLEMENTATION_H
+
+
+namespace glrt {
+namespace renderer {
+
+class GpuVoxelizerImplementation
+{
+public:
+  GpuVoxelizerImplementation();
+};
+
+} // namespace renderer
+} // namespace glrt
+
+#endif // GLRT_RENDERER_GPUVOXELIZERIMPLEMENTATION_H
---
+++ gl-research-toolkit/renderer/src/glrt/renderer/toolkit/gpu-voxelizer-implementation.cpp
@@ -0,0 +1,11 @@
+#include <glrt/renderer/toolkit/gpu-voxelizer-implementation.h>
+
+namespace glrt {
+namespace renderer {
+
+GpuVoxelizerImplementation::GpuVoxelizerImplementation()
+{
+}
+
+} // namespace renderer
+} // namespace glrt
