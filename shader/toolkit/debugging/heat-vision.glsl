e619add4fe065b9981fc16f27ce501d2917495c7
Robert Hildebrandt <robert-42@web.de>
2016-05-05 17:32:52 +0200

    improved the heatvision colors

--- shader/toolkit/debugging/heat-vision.glsl
+++ shader/toolkit/debugging/heat-vision.glsl
@@ -5,17 +5,18 @@ vec3 heatvision_color_grey(float normalized_value)
 
 vec3 heatvision_color(float normalized_value)
 {
-  vec3 c[7];
+  vec3 c[8];
   
   c[0] = vec3(0);
-  c[1] = vec3(0.014, 0.068, 0.242); // #204a87
-  c[2] = vec3(0.178, 0.08, 0.198); // #75507b
-  c[3] = vec3(0.604, 0, 0); // #cc0000
-  c[4] = vec3(0.913, 0.191, 0); // #f57900
-  c[5] = vec3(0.973, 0.815, 0.001); // #fce94f
-  c[6] = vec3(1); // #
+  c[1] = vec3(0.014, 0.068, 0.242)*0.6; // #204a87
+  c[2] = vec3(0.107, 0.036, 0.133)*1.25; // #5c3566
+  c[3] = vec3(0.178, 0.08, 0.198); // #75507b
+  c[4] = vec3(0.604, 0, 0); // #cc0000
+  c[5] = vec3(0.913, 0.191, 0); // #f57900
+  c[6] = vec3(0.973, 0.815, 0.001); // #fce94f
+  c[7] = vec3(1); // #
   
-  float value = clamp(normalized_value * 6, 0, 6);
+  float value = clamp(normalized_value * 7, 0, 7);
   
   return mix(c[int(floor(value))],
              c[int(ceil(value))],
