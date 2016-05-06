3929c2f7780bb96085f53333ec336aa440f8c85d
Robert Hildebrandt <robert-42@web.de>
2016-05-06 22:05:11 +0200

    addded comment

--- gl-research-toolkit/scene/include/glrt/scene/resources/voxelizer.h
+++ gl-research-toolkit/scene/include/glrt/scene/resources/voxelizer.h
@@ -34,6 +34,7 @@ public:
   struct Hints
   {
     utilities::GlTexture::Type voxelType = utilities::GlTexture::Type::FLOAT16;
+    // #TODO: test with extend=1
     float extend = 2.f;
     int minSize = 4;
     int maxSize = 64; // #FIXME: why doesn't 128 work for sponza?
