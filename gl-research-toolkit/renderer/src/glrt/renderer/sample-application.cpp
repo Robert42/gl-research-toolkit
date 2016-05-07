1b1d4d1dc063caa03a0d0db7e53d9341053d4c0e
Robert Hildebrandt <robert-42@web.de>
2016-05-07 20:24:47 +0200

    added note

--- gl-research-toolkit/renderer/src/glrt/renderer/sample-application.cpp
+++ gl-research-toolkit/renderer/src/glrt/renderer/sample-application.cpp
@@ -22,6 +22,7 @@ SampleApplication::SampleApplication(int& argc, char** argv,
   // #FIXME: reproducible crash if uncommenting the following two lines, while having the cornellBoxSuzanneScene() as the default scene (see sampleApplicationSettings.sceneToLoad)
   qApp->processEvents();
   scene.unloadUnusedResources();
+  // #TODO::::: why is not revoxelizing having the right result right away?
 
   scene.load(sampleApplicationSettings.sceneToLoad);
 }
