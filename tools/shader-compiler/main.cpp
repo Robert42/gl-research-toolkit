d10b7366dc79f711704c4ec526b40043ab3affbd
Robert Hildebrandt <robert-42@web.de>
2016-05-09 17:16:05 +0200

    started implementing shader-compiler tool

---
+++ tools/shader-compiler/main.cpp
@@ -0,0 +1,33 @@
+#include <glrt/application.h>
+#include <glrt/gui/toolbar.h>
+
+#include <glhelper/gl.hpp>
+
+
+int main(int argc, char** argv)
+{
+  glrt::Application app(argc, argv, glrt::System::Settings::simpleWindow("Shader-Compiler"));
+
+
+  app.showWindow();
+
+  while(app.isRunning)
+  {
+    SDL_Event event;
+    while(app.pollEvent(&event))
+    {
+      // T O D O : Add your event handling code here
+    }
+
+    app.update();
+
+    GL_CALL(glClear, GL_COLOR_BUFFER_BIT);
+
+    // T O D O : Add your drawing code here
+
+    app.swapWindow();
+  }
+
+
+  return 0;
+}
