c5c188930157067abbafe8585a5d173190fdc0cc
Robert Hildebrandt <robert-42@web.de>
2016-05-25 19:22:56 +0200

    fix

--- shader/materials/implementation/forward-lighting.fs.glsl
+++ shader/materials/implementation/forward-lighting.fs.glsl
@@ -66,7 +66,7 @@ return;
 #if defined(DISTANCEFIELD_AO)
   uint32_t stepCount = 0;
   GlobalDistanceField global_distance_field = init_global_distance_field();
-  float ao = distancefield_ambientocclusion(global_distance_field, surface.position, surface.normal, stepCount);
+  float ao = distancefield_ambientocclusion(global_distance_field, surface.position, material.normal, stepCount);
   fragment_color = vec4(vec3(ao), 1);
   return; 
 #endif
