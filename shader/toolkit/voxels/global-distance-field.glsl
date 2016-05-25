fe9605ec72e71a6beb52326baef1df7b8f79d1ae
Robert Hildebrandt <robert-42@web.de>
2016-05-25 18:16:55 +0200

    avoiding issues because of nagative voxel values not being enclosed

--- shader/toolkit/voxels/global-distance-field.glsl
+++ shader/toolkit/voxels/global-distance-field.glsl
@@ -33,7 +33,11 @@ float distance_to_location(const in GlobalDistanceField globalDistanceField, vec
     const float distance_within_voxel = distancefield_distance(location_voxelspace, spaceFactor, globalDistanceField.distance_field_textures[i]);
     const float distance_to_voxelgrid = spaceFactor.voxelToWorldSpace * distance(location_voxelspace, clamped_location_voxelspace);
     
-    float voxelSpaceDistance = distance_within_voxel + distance_to_voxelgrid;
+    // The following lines contains the following workaround:
+    // if distance_to_voxelgrid>0, the location_ws is outside the bounding box. So clamp distance_within_voxel to [0,inf) (in case the shape is not closed, like the columns. Not clamping will result in the algorithm thinking, the whole area below and above the column is below the surface, as it's negative)
+    // Otherwise, just use the distance_within_voxel
+    float voxelSpaceDistance = distance_to_voxelgrid > 1.e-4f ? max(0,distance_within_voxel) + distance_to_voxelgrid : distance_within_voxel;
+    
     float worldSpaceDistance = spaceFactor.voxelToWorldSpace * voxelSpaceDistance;
     
     d = min(d, worldSpaceDistance);
@@ -52,11 +56,11 @@ float distance_to_location(const in GlobalDistanceField globalDistanceField, vec
 bool raymarch(const in GlobalDistanceField globalDistanceField, in Ray ray, out vec3 world_pos, inout uint32_t stepCount)
 {
   const float clip_front = 0.001f;
-  const float clip_back = 200.f;
+  const float clip_back = 50.f;
   
   float t = clip_front; 
   
-  int max_num_loops = 65536;
+  int max_num_loops = 1024;
   while(t < clip_back && 0<=max_num_loops--)
   {
     ++stepCount;
@@ -73,6 +77,8 @@ bool raymarch(const in GlobalDistanceField globalDistanceField, in Ray ray, out 
     
     t += d;
   }
+  
+  return false;
 }
 
 vec3 distancefield_normal(const in GlobalDistanceField globalDistanceField, vec3 location_ws, inout uint32_t stepCount, float epsilon=0.01f)
