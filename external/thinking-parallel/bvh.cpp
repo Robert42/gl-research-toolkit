d2d5df1d294118b4d3f6fb036ddbe346c900e569
Robert Hildebrandt <robert-42@web.de>
2016-08-18 10:16:13 +0200

    added debug output for the split finder

--- external/thinking-parallel/bvh.cpp
+++ external/thinking-parallel/bvh.cpp
@@ -45,25 +45,46 @@ inline quint32 BVH::zIndexDistance(quint16 a, quint16 b)
 // see also https://devblogs.nvidia.com/parallelforall/thinking-parallel-part-iii-tree-construction-gpu/
 quint16 BVH::findSplit(quint16 begin, quint16 end)
 {
-  quint16 min_split_value=begin+1;
-  quint16 max_split_value=end-1;
+#define PRINT_SPLITS 0
 
-  quint16 split = (begin+end)/2;
+#if PRINT_SPLITS
+  qDebug() << "\n\n\nBVH::findSplit("<<begin<<"," << end << ")";
+  QString str1;
+  for(quint16 i=begin; i<end; ++i)
+    str1 += QString("0x%0 ").arg(QString::number(leaves_z_indices[i], 16));
+  qDebug() << "  before split:\n"<<str1.toStdString().c_str();
+#endif
 
-  while(begin+1 < end)
+  quint16 range_begin=begin;
+  quint16 range_end=end;
+
+  quint16 split = (range_begin+end)/2;
+
+  while(range_begin+1 < range_end)
   {
     Q_ASSERT(split>0);
-    Q_ASSERT(end>0);
+    Q_ASSERT(range_end>0);
 
-    if(zIndexDistance(begin, split-1) > zIndexDistance(split, end-1))
-      end = split;
+    if(zIndexDistance(range_begin, split-1) > zIndexDistance(split, range_end-1))
+      range_end = split;
     else
-      begin = split;
+      range_begin = split;
 
-    split = (begin+end)/2;
+    split = (range_begin+range_end)/2;
   }
 
-  return glm::clamp<quint16>(split, min_split_value, max_split_value);
+  split = glm::clamp<quint16>(split, begin+1, end-1);
+
+#if PRINT_SPLITS
+  QString str2;
+  for(quint16 i=begin; i<split; ++i)
+    str1 += QString("0x%0 ").arg(QString::number(leaves_z_indices[i], 16));
+  for(quint16 i=split; i<end; ++i)
+    str1 += QString("0x%0 ").arg(QString::number(leaves_z_indices[i], 16));
+  qDebug() << "  after split:\n"<<str1.toStdString().c_str()<<"  ||  "<<str2.toStdString().c_str();
+#endif
+
+  return split;
 }
 
 } // namespace renderer
