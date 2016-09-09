4c5df3830481c45bb881f57e91fd9075797ccae1
Robert Hildebrandt <robert-42@web.de>
2016-09-09 08:50:42 +0200

    the benchmark can now accept arguments

--- samples/benchmark/main.cpp
+++ samples/benchmark/main.cpp
@@ -4,6 +4,72 @@ int main(int argc, char** argv)
 {
   const glm::uvec2 resolution(1920, 1080);
 
+  int max_num_frames = 1000;
+  float max_time = 30.f;
+  bool deferred = false;
+
+  QVector<float> all_frame_times;
+  QMap<QString, QString> gui_commands;
+
+  QStringList arguments;
+  for(int i=1; i<argc; ++i)
+    arguments << QString::fromUtf8(argv[i]).trimmed();
+
+  while(!arguments.isEmpty())
+  {
+    QString argument = arguments.first();
+
+    bool ok = false;
+    if(arguments.first() == "--gui")
+    {
+      arguments.removeFirst();
+
+      if(arguments.length() >= 2)
+      {
+        ok = true;
+        gui_commands[arguments[0]] = arguments[1];
+        arguments.removeFirst();
+        arguments.removeFirst();
+      }
+    }else if(arguments.first() == "--max_num_frames")
+    {
+      arguments.removeFirst();
+
+      if(arguments.length() >= 1)
+      {
+        max_time = arguments.first().toFloat(&ok);
+        arguments.removeFirst();
+      }
+    }else if(arguments.first() == "--max_num_frames")
+    {
+      arguments.removeFirst();
+
+      if(arguments.length() >= 1)
+      {
+        max_num_frames = arguments.first().toInt(&ok);
+        arguments.removeFirst();
+      }
+    }else if(arguments.first() == "--deferred")
+    {
+      arguments.removeFirst();
+      ok = true;
+      deferred = true;
+    }else if(arguments.first() == "--forward")
+    {
+      arguments.removeFirst();
+      ok = true;
+      deferred = false;
+    }
+
+    if(!ok)
+    {
+      qWarning() << "invalid argument" << argument;
+      return 1;
+    }
+  }
+
+  all_frame_times.reserve(max_num_frames);
+
   glrt::SampleApplication app(argc, argv,
                               glrt::gui::AntTweakBar::Settings::sampleGui("This Sample shows how to use the forward renderer to render a simple scene" // help text of the sample
                                                                           ),
@@ -11,8 +77,21 @@ int main(int argc, char** argv)
                               glrt::Application::Settings::techDemo(),
                               glrt::System::Settings::addVSync(glrt::System::Settings::fullscreen("Benchmark", resolution), false));
 
+  // TODO: app.antweakbar.showAllPanels(); // to make sure all commands succeed
+  for(const QString& key : gui_commands.keys())
+  {
+    // TODO: app.antweakbar.setValue(key, gui_commands.value(key));
+  }
+
+  app.antweakbar.visible = false;
+
+  SDL_ShowCursor(0);
+
   app.showWindow();
 
+  int num_frames = 0;
+  float total_time = 0;
+  bool aborted_by_criteria = false;
   while(app.isRunning)
   {
     SDL_Event event;
@@ -34,7 +113,11 @@ int main(int argc, char** argv)
 
     app.endDrawing();
     app.swapWindow();
+
+    all_frame_times << deltaTime;
+    aborted_by_criteria = (num_frames++) >= max_num_frames || (total_time+=deltaTime) > max_time;
+    app.isRunning = app.isRunning && !aborted_by_criteria;
   }
 
-  return 0;
+  return aborted_by_criteria ? 0 : 42;
 }
