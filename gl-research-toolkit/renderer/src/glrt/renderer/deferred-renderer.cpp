f20d2ef27218263f885bb5ddc9370a2293ede9f7
Robert Hildebrandt <robert-42@web.de>
2016-09-11 13:41:26 +0200

    refactoring

--- gl-research-toolkit/renderer/src/glrt/renderer/deferred-renderer.cpp
+++ gl-research-toolkit/renderer/src/glrt/renderer/deferred-renderer.cpp
@@ -70,7 +70,7 @@ void DeferredRenderer::prepareFramebuffer()
 {
   mrt_framebuffer.Bind(true);
 
-  glClearColor(0.f, 0.f, 0.f, 1.f);
+  glClearColor(0.f, 0.f, 0.f, 0.f);
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 }
 
