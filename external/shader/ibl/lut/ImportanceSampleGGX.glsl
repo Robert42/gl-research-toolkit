d7a16625fc55affb9d5be27f711618e31cdef60d
Robert Hildebrandt <robert-42@web.de>
2016-12-30 14:11:03 +0100

    converted hlsl to glsl

--- external/shader/ibl/lut/ImportanceSampleGGX.glsl
+++ external/shader/ibl/lut/ImportanceSampleGGX.glsl
@@ -1,18 +1,18 @@
-float3 ImportanceSampleGGX( float2 Xi, float Roughness , float3 N )
+vec3 ImportanceSampleGGX(vec2 Xi, float Roughness, vec3 N)
 {
   float a = Roughness * Roughness;
   float Phi = 2 * PI * Xi.x;
   float CosTheta = sqrt( (1 - Xi.y) / ( 1 + (a*a - 1) * Xi.y ) );
   float SinTheta = sqrt( 1 - CosTheta * CosTheta );
   
-  float3 H;
+  vec3 H;
   H.x = SinTheta * cos( Phi );
   H.y = SinTheta * sin( Phi );
   H.z = CosTheta;
   
-  float3 UpVector = abs(N.z) < 0.999 ? float3(0,0,1) : float3(1,0,0);
-  float3 TangentX = normalize( cross( UpVector , N ) );
-  float3 TangentY = cross( N, TangentX );
+  vec3 UpVector = abs(N.z) < 0.999 ? vec3(0,0,1) : vec3(1,0,0);
+  vec3 TangentX = normalize( cross( UpVector , N ) );
+  vec3 TangentY = cross( N, TangentX );
   // Tangent to world space
   return TangentX * H.x + TangentY * H.y + N * H.z;
 }
