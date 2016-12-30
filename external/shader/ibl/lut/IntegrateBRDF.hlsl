4aebe1d43f2960034bbee8b6d1e44da0424bfc2d
Robert Hildebrandt <robert-42@web.de>
2016-12-30 10:43:38 +0100

    added hlsl shader for calculating the dfg lut

---
+++ external/shader/ibl/lut/IntegrateBRDF.hlsl
@@ -0,0 +1,34 @@
+float2 IntegrateBRDF( float Roughness , float NoV )
+{
+  float3 V;
+  V.x = sqrt( 1.0f - NoV * NoV ); // sin
+  V.y = 0;
+  V.z = NoV;
+  
+  // cos
+  float A = 0;
+  float B = 0;
+  
+  const uint NumSamples = 1024;
+  for( uint i = 0; i < NumSamples; i++ )
+  {
+    float2 Xi = Hammersley( i, NumSamples );
+    float3 H = ImportanceSampleGGX( Xi, Roughness , N );
+    float3 L = 2 * dot( V, H ) * H - V;
+    
+    float NoL = saturate( L.z );
+    float NoH = saturate( H.z );
+    float VoH = saturate( dot( V, H ) );
+    
+    if( NoL > 0 )
+    {
+      float G = G_Smith( Roughness , NoV, NoL );
+      float G_Vis = G * VoH / (NoH * NoV);
+      float Fc = pow( 1 - VoH, 5 );
+      A += (1 - Fc) * G_Vis;
+      B += Fc * G_Vis;
+    }
+  }
+  
+  return float2( A, B ) / NumSamples;
+}
