082b52dff32ead63de656ea35d8a3054f6a8ffbd
Robert Hildebrandt <robert-42@web.de>
2016-12-31 15:08:34 +0100

    fix

--- external/shader/pbs/pbs.glsl
+++ external/shader/pbs/pbs.glsl
@@ -8,7 +8,6 @@
 #include "../mrp/mrp.glsl"
 #include "luminance-for-luminous-power.glsl"
 #include "camera.glsl"
-#include "image-based-lighting-precalculation.glsl"
 #include "image-based-lighting.glsl"
 
 #undef saturate
