18f094c74cd6351f3cdf3742e294e321a06a894b
Robert Hildebrandt <robert-42@web.de>
2017-01-07 09:43:55 +0100

    continued

--- shader/compute/ibl.cs
+++ shader/compute/ibl.cs
@@ -8,6 +8,7 @@ layout(local_size_x=GROUPS_SIZE_X, local_size_y=GROUPS_SIZE_Y, local_size_z=GROU
 struct IblHeader
 {
   layout(r16f) writeonly image2D targetTexture;
+  sampler2D sourceTexture;
   mat4 cube_side;
 };
 
@@ -16,3 +17,9 @@ uniform IblHeaderBlock
 {
   IblHeader header;
 };
+
+void main()
+{
+  ivec2 texture_coordinate = ivec2(gl_GlobalInvocationID.xy);
+  imageStore(header.targetTexture, texture_coordinate, vec4(1.0, 0.5, 0.0, 1.0));
+}
