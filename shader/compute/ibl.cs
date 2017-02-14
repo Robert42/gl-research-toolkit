c6760c7d3a185d08721d74266c22d9f491b64772
Robert Hildebrandt <robert-42@web.de>
2017-02-14 10:40:18 +0100

    refactoring

--- shader/compute/ibl.cs
+++ shader/compute/ibl.cs
@@ -92,27 +92,7 @@ vec4 sample_environment(vec3 view, float mipLevel)
   return vec4(incoming, 1);
 }
 
-// source https://en.wikipedia.org/w/index.php?title=Low-discrepancy_sequence&oldid=731052708#Hammersley_set
-vec2 Hammersley(uint n, uint N)
-{
-  float g_2 = 0.f;
-  float base = 0.5;
-
-  for(uint k=1; k<N; k<<=1)
-  {
-    if((n & k) != 0)
-      g_2 += base;
-    base *= 0.5;
-  }
-
-  return vec2(g_2, double(n)/double(N));
-}
-
-
-vec2 getSample(uint i, uint sampleCount)
-{
-  return Hammersley(i, sampleCount);
-}
+#include <random.glsl>
 
 vec3 integrateCone(vec3 N, float cone_angle, int sampleCount)
 {
